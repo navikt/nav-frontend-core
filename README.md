eb6023f8b7e43b1e1b0a66d6fd6476433d1af9fa
Helge Fredheim <helge.fredheim@nav.no>
2016-11-28 13:49:08 +0100

    Hello world for modig-frontend-core

---
+++ README.md
@@ -0,0 +1,10 @@
+#NAV-frontend-core
+
+Variabler og andre felleselementer for NAV-frontend. 
+
+For å kjøre
+
+```
+npm install
+npm run watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+var stylesPath = path.resolve(__dirname, 'less', 'styles.less');
+var mainPath = path.resolve(__dirname, 'index.js');
+
+module.exports = {
+    entry: [mainPath, stylesPath],
+    output: {
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [{
+            test: /\.less$/,
+            loader: "style-loader!css-loader!less-loader"
+        }]
+    }
+}
